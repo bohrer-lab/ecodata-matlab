7f8423ecc8bd83a6f2944a841038efe5752611e8
Justine Missik <missik.2@osu.edu>
2022-03-13 22:57:00 -0400

    Move dataset utils to new file

--- pymovebank/src/pymovebank/__init__.py
+++ pymovebank/src/pymovebank/__init__.py
@@ -1,2 +1,10 @@
-from pymovebank.functions import grib2nc, subset_data, get_extent, get_crs, get_file_info, get_geometry, get_file_len
 from pymovebank.datasets import *
+from pymovebank.functions import (
+    get_crs,
+    get_extent,
+    get_file_info,
+    get_file_len,
+    get_geometry,
+    grib2nc,
+    subset_data,
+)
