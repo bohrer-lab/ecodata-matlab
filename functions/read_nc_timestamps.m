8fecaeb0066d16b926d37a274d4ed47a26bfb438
Justine Missik <missik.2@osu.edu>
2022-11-08 20:03:06 -0500

    Update handling of nc timestamps - move reading timestamps to a separate function - check for NASA or ECMWF format and parse the timestamp appropriately

---
+++ functions/read_nc_timestamps.m
@@ -0,0 +1,20 @@
+function nc_timestamp = read_nc_timestamps(nc_filename, timevar)
+    % Checks the time format of the dataset, and produces a datetime
+    % accordingly 
+    % Checks for ECMWF format and NASA format
+    % Other options not yet supported 
+
+    % Read the time metadata 
+    time_units = ncreadatt(nc_filename, timevar, 'units');
+    netcdf_time = ncread(nc_filename, timevar);
+
+    % ECMWF format 
+    if startsWith(time_units, 'hours since 1900-01-01')
+        nc_timestamp = datetime(datevec(double(netcdf_time)/24 + datenum('1900-01-01 00:00:00')));
+
+    % NASA format 
+    elseif startsWith(time_units, 'days since 2000-01-01')
+        nc_timestamp = datetime(datevec(double(netcdf_time + datenum('2000-01-01 00:00:00'))));
+    
+    end
+end
