dc14f2fcf1ba4b54bbb4d4848d34fe361b31b538
Justine Missik <missik.2@osu.edu>
2022-11-15 23:39:45 -0500

    change groupby_individual_and_resample to output map

--- functions/group_by_individual_and_resample.m
+++ functions/group_by_individual_and_resample.m
@@ -1,4 +1,4 @@
-function [inds,c] = group_by_individual_and_resample(data, resample_interval)
+function groups = group_by_individual_and_resample(data, resample_interval)
     % Group track data by individual_local_identifier and resample to a specified time frequency  
     %
     % Args:
@@ -13,13 +13,24 @@ function [inds,c] = group_by_individual_and_resample(data, resample_interval)
     data2 = data(:, {'location_lat', 'location_long', 'individual_local_identifier'});
     inds = unique(data2.individual_local_identifier);
 
-    c = cell(1,length(inds));
-    for i = 1:length(c), c{1,i} = {inds{i},data2(strcmp(data2.individual_local_identifier, inds{i}), :)}; end
+%     c = cell(1,length(inds));
+%     for i = 1:length(c), c{1,i} = {inds{i},data2(strcmp(data2.individual_local_identifier, inds{i}), :)}; end
+      groups = containers.Map();
+      for i=1:length(inds)
+          groups(inds{i}) = data2(strcmp(data2.individual_local_identifier, inds{i}), :);
+      end
 
+%     %resample
+%     for i = 1:length(c)
+%         c{1,i}{2} = sortrows(c{1,i}{2}, 'timestamp');
+%         c{1,i}{2} = resample_track_data(c{1,i}{2}(:, {'location_lat', 'location_long'}), resample_interval); 
+%     end
     %resample
-    for i = 1:length(c)
-        c{1,i}{2} = sortrows(c{1,i}{2}, 'timestamp');
-        c{1,i}{2} = resample_track_data(c{1,i}{2}(:, {'location_lat', 'location_long'}), resample_interval); 
+    for i = 1:length(inds)
+        dat = sortrows(groups(inds{i}), 'timestamp');
+        dat = resample_track_data(dat(:, {'location_lat', 'location_long'}), resample_interval);
+        groups(inds{i}) = dat; 
     end
+
 
 end
