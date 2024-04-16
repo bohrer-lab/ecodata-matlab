858117e2271459e1371990195fe0e2d490a26645
Justine Missik <missik.2@osu.edu>
2024-04-15 20:09:41 -0400

    Add group_by option to labeled points class

--- functions/LabeledPoints.m
+++ functions/LabeledPoints.m
@@ -3,11 +3,13 @@ classdef LabeledPoints < handle
         filepath
         data
         marker_size
-        marker_color
+        marker_color  % Ignored if using group_by
         text_size
         text_color
         show_text
-        
+        group_by
+        cmap  % Colors if using group_by
+        group_colors
     end
 
     methods
@@ -21,6 +23,8 @@ classdef LabeledPoints < handle
                 kwargs.text_size = 8
                 kwargs.text_color = 'k'
                 kwargs.show_text = true
+                kwargs.group_by = NaN
+                kwargs.cmap = lines
             end
 
             if nargin > 0
@@ -30,8 +34,32 @@ classdef LabeledPoints < handle
                 obj.text_size = kwargs.text_size;
                 obj.text_color = kwargs.text_color;
                 obj.show_text = kwargs.show_text;
+                obj.group_by = kwargs.group_by;
+                obj.cmap = kwargs.cmap;
 
                 obj.data = LabeledPoints.prepare_labels(filepath);
+
+                if ~isnan(obj.group_by)
+                    % Make sure groups are not numeric
+                    if isnumeric(obj.data.(obj.group_by))
+                        obj.data.(obj.group_by) = cellstr(string(obj.data.(obj.group_by)));
+                    end
+
+                    % Prepare colors if using group_by
+                    group_labels = unique(obj.data.(obj.group_by));
+                    obj.group_colors = containers.Map();
+                    point_colors = repmat(obj.cmap, ceil(length(group_labels)/length(obj.cmap)),1);
+
+                    % Create color mapping 
+                    for i=1:length(group_labels)
+                        obj.group_colors(group_labels{i}) = point_colors(i, :);
+                    end
+
+                    % Add colors to table
+                    for i=1:height(obj.data)
+                        obj.data.("marker_color")(i) = {obj.group_colors(obj.data.(obj.group_by){i})};
+                    end
+                end
             end
         end
 
@@ -60,10 +88,29 @@ classdef LabeledPoints < handle
 
             % Select only the point that should be visible at the frame time step
             labels_filtered = obj.data(frame_time>=obj.data.start_time & frame_time<=obj.data.end_time,:);
+            [labels_filtered.m_longitude, labels_filtered.m_latitude] = m_ll2xy(labels_filtered.longitude, labels_filtered.latitude);
 
             % Plot the points
-            m_scatter(labels_filtered.longitude, labels_filtered.latitude, ...
-                obj.marker_size, obj.marker_color, 'filled')
+            if isnan(obj.group_by)
+                color = obj.marker_color;
+            else
+                color = cell2mat(labels_filtered.("marker_color"));
+                % Create legend items for each group
+                % group_labels = obj.group_colors.keys;
+                % legend_items = gobjects(length(group_labels),1);
+                % for l=1:length(legend_items)
+                %     legend_items(l) = scatter(nan, nan, obj.marker_size, obj.group_colors(group_labels{l}), 'filled');
+                % end
+                % legend(legend_items, group_labels)
+                % legend
+            end
+            scatter(labels_filtered.m_longitude, labels_filtered.m_latitude, obj.marker_size, color, "filled");
+
+            % ylabel('') 
+            % xlabel('')
+            % legend
+            % end
+
 
             % Add labels
             if obj.show_text
@@ -80,7 +127,7 @@ classdef LabeledPoints < handle
         function labels = prepare_labels(filepath)
 
             opts = detectImportOptions(filepath);
-            opts = setvartype(opts, {'label'}, {'char'});
+            opts = setvartype(opts, {'label'}, 'char');
         
             labels = readtable(filepath, opts);
         
